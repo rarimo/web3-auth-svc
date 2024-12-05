fe5d3a2e35ab7796007c56627cb1ff93e249b07b
Maksym Hrynenko <mg06062003@gmail.com>
2024-12-05 11:56:33 +0200

    update (resources): with admin authorization docs

---
+++ resources/model_authorize_admin_attributes.go
@@ -0,0 +1,10 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+type AuthorizeAdminAttributes struct {
+	// Password to get admin permissions
+	Password string `json:"password"`
+}
--- resources/model_claim.go
+++ resources/model_claim.go
@@ -8,4 +8,6 @@ package resources
 type Claim struct {
 	// Address authorized with
 	Address string `json:"address"`
+	// Whether the user has a admin permissions
+	IsAdmin bool `json:"is_admin"`
 }
